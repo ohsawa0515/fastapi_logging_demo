909b4e44a25f33b8d4e45f9bd18bccb835d98ea3
Shuichi Ohsawa <ohsawa0515@gmail.com>
2021-01-29 23:55:47 +0900

    logging context include request and response body by APIRoute

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import time
+import datetime
 import sys
 import json
 from logging import getLogger, StreamHandler, DEBUG
-from fastapi import FastAPI, Request
+from typing import Callable
+from fastapi import FastAPI, Request, Response
+from fastapi.routing import APIRoute
 from pydantic import BaseModel
 
 
@@ -12,34 +15,47 @@ handler.setLevel(DEBUG)
 logger.addHandler(handler)
 logger.setLevel(DEBUG)
 
+
+class LoggingContextRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            before = time.time()
+            response: Response = await original_route_handler(request)
+            duration = round(time.time() - before, 4)
+
+            record = {}
+            time_local = datetime.datetime.fromtimestamp(before)
+            record["time_local"] = time_local.strftime("%Y/%m/%d %H:%M:%S%Z")
+            if await request.body():
+                record["request_body"] = (await request.body()).decode("utf-8")
+            record["request_headers"] = {
+                k.decode("utf-8"): v.decode("utf-8") for (k, v) in request.headers.raw
+            }
+            record["remote_addr"] = request.client.host
+            record["request_uri"] = request.url.path
+            record["request_method"] = request.method
+            record["request_time"] = str(duration)
+            record["status"] = response.status_code
+            record["response_body"] = response.body.decode("utf-8")
+            record["response_headers"] = {
+                k.decode("utf-8"): v.decode("utf-8") for (k, v) in response.headers.raw
+            }
+            logger.info(json.dumps(record))
+            return response
+
+        return custom_route_handler
+
+
 app = FastAPI()
+app.router.route_class = LoggingContextRoute
 
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: bool = None
-
-
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = round(time.time() - start_time, 4)
-
-    record = {}
-    record["request_time"] = str(process_time)
-    record["remote_addr"] = request.client.host
-    record["server_port"] = request.client.port
-    record["request_uri"] = request.url.path
-    record["request_method"] = request.method
-    record["status"] = response.status_code
-    record["response_headers"] = {
-        k.decode("utf-8"): v.decode("utf-8")
-        for (k, v) in response.headers.raw
-    }
-    logger.info(json.dumps(record))
-    return response
 
 
 @app.get("/")
